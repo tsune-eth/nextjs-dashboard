aa1e7543adb7294f228f1afde11c3d74bea7fbaa
tsune-eth <tsunekun@yicr.info>
2023-11-26 15:55:52 +0900

    second

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Vina_Sans } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const vina_sans = Vina_Sans({ subsets: ['latin'], weight: ['400'] });
